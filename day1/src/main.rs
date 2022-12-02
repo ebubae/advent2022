668bea98b43ae88cf2c86e7c31a8ac079089cbe5
Ebube Chuba <ebube97@me.com>
2022-12-02 13:00:50 -0500

    day1 p2 rust submission

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -10,10 +10,21 @@ fn main() {
                 .map(|elf_item| elf_item.parse::<i32>().unwrap())
                 .sum()
         })
-        // .flatten()
         .max()
         .unwrap_or(0);
     println!("part 1: {:?}", result);
-    let result2 = full_string.trim();
-    println!("part 2: {:?}", result2);
+    let mut tops = full_string
+        .trim()
+        .split("\n\n")
+        .map(|elf| {
+            elf.split("\n")
+                .map(|elf_item| elf_item.parse::<i32>().unwrap())
+                .sum()
+        })
+        .collect::<Vec<i32>>();
+    tops.sort();
+    tops.reverse();
+    if let Some(slice) = tops.get(0..3) {
+        println!("part 2: {:?}", slice.iter().sum::<i32>());
+    }
 }
